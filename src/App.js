13157abb043c4068d30b070d9632b0ce21ca2795
joel <joel_zerg_pro@outlook.com>
2021-10-30 21:28:22 -0500

    dev_2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. SIUUUUUUUUUUUUUUUUUUUUUUUUUUUU
+          Edit <code>src/App.js</code> and save to reload.fff SIUUUUUUUUUUUUUUUUUUUUUUUUUUUU
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ssss
+          learn react
         </a>
       </header>
     </div>
