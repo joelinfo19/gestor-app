bdcd07d47310279a88b453e2e4affd6f452e0861
joel <joel_zerg_pro@outlook.com>
2021-10-30 21:45:46 -0500

    test_error

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.ffffsfsfsfsdf SIUUUUUUUUUUUUUUUUUUUUUUUUUUUU
+          Edit <code>src/App.js</code> and save to reload.ffffsfsfsfsdfsdaad SIUUUUUUUUUUUUUUUUUUUUUUUUUUUU
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          lesdadast
         </a>
       </header>
     </div>
