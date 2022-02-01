34a57a7ef37323b3eb1cb0e9c737fd77a2260ba0
José Ángel <joseyanez2298@gmail.com>
2022-01-31 22:09:12 -0500

    matriz alumnos

--- src/components/EstadisticasAlumnos.js
+++ src/components/EstadisticasAlumnos.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import ReactAnime from 'react-animejs'
 import {helpHttp} from '../helpers/helpHttp';
+import { CSVLink, CSVDownload } from 'react-csv';
 
 const {Anime, stagger} = ReactAnime
 
@@ -8,13 +9,32 @@ const {Anime, stagger} = ReactAnime
 const uri = 'https://testunsaac.herokuapp.com/api/matriculas/';
 const api = helpHttp();
 
+const csvData = [
+  ["firstname", "lastname", "email"],
+  ["Ahmed", "Tomi", "ah@smthing.co.com"],
+  ["Raed", "Labes", "rl@smthing.co.com"],
+  ["Yezzi", "Min l3b", "ymin@cocococo.com"]
+];
+
 export const EstadisticasAlumnos = ({idMatricula}) => {
 
   const [porcentajeAsisenciasAlumnos, setPorcentajeAsistenciasAlumnos] = useState(null);
+  const [matrizAsistenciasAlumnos, setMatrizAsistenciasAlumnos] = useState(null);
 
   const porcentajeAsistencias = (arrAsistencias) => {
     let totalAsistencias = arrAsistencias.reduce((acc, el) => el.flag ? acc + 1 : acc , 0 );
     return (totalAsistencias/arrAsistencias.length) * 100;
+  }
+
+  const matrizAsistencias = (arrAlumnos) => {
+    let arrClases = ["Alumno"];
+    arrClases.push(...arrAlumnos[0].asistencias.map((el, index) => `Clase${index} ${el.date.substring(0, 10)}`));
+    let arrAsistenciasAlumnos = arrAlumnos.map( (el) => {
+      let filaAsistencias = [el.nombre];
+      filaAsistencias.push(...el.asistencias.map(elem => elem.flag ? "Asistió": "Faltó"));
+      return filaAsistencias;
+    })
+    setMatrizAsistenciasAlumnos([arrClases, ...arrAsistenciasAlumnos])
   }
 
   useEffect(() => {
@@ -23,16 +43,16 @@ export const EstadisticasAlumnos = ({idMatricula}) => {
         if(matricula.ok){
           let porcentajeAlumnos = matricula.matriculas.alumno.map(x => {return {...x, asistencias:porcentajeAsistencias(x.asistencias)}});
           setPorcentajeAsistenciasAlumnos(porcentajeAlumnos);
-          console.log(porcentajeAlumnos)
+          matrizAsistencias(matricula.matriculas.alumno)
         }
       })
       .catch(err => console.log(err))
   }, [])
 
   return (
-    <div style={{width: '80%'}}>
+    <div style={{width: '90%', margin: '1rem', display: 'flex', justifyContent: 'center', flexWrap:'wrap'}}>
       { porcentajeAsisenciasAlumnos ? 
-        <div>
+        <div style={{width: '90%'}}>
             {porcentajeAsisenciasAlumnos.map((el, index) => 
           <div style={{marginBottom: '25px'}}>
             <Anime _onUpdate={[
@@ -46,7 +66,7 @@ export const EstadisticasAlumnos = ({idMatricula}) => {
               }
             ]}>
               <div>{el.nombre}</div>
-              <div key={el._id} id={`a${index}`} style={{height: '20px', width: '3px', backgroundColor: '5a87ff', textAlign: 'center'}}><b>{el.asistencias}%</b></div>
+              <div key={el._id} id={`a${index}`} style={{height: '23px', width: '3px', backgroundColor: '#5a87ff', textAlign: 'center', borderRadius: '3px'}}><b>{el.asistencias ? Math.round(el.asistencias*100, 2)/100 : 0}%</b></div>
             </Anime>
           </div>
             )}
@@ -54,6 +74,8 @@ export const EstadisticasAlumnos = ({idMatricula}) => {
         :
         <div>Cargando</div>
       }
+      <div style={{width:'100%'}}><b>Otros reportes: </b></div>
+      {matrizAsistenciasAlumnos && <CSVLink data={matrizAsistenciasAlumnos} filename={"Asistencias.csv"} style={{color: '#9a57ff', width: '200px', backgroundColor: '#61c3ff', borderRadius:'5px', textAlign:'center', textDecoration:'none'}}>Descargar matriz de asistencias</CSVLink>}
     </div>
   )
 }
